95656a079816b652e25b21abbdab3fa55beba531
Maria Elsa Tortello Ditta <maria.tortelloditta2@gmail.com>
2024-05-30 19:44:32 -0500

    starting JS_DOM manipulation

---
+++ scripts/aroundUS_proj5Spt5.js
@@ -0,0 +1,5 @@
+let content = document.querySelector(".content");
+let editProfileBtn = content.querySelector(".profile__info-editNamePencil");
+let addImgBtn = content.querySelector(".profile__addProfile-sign");
+
+//console.log(editProfileBtn);
