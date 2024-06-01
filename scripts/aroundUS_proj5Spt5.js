d82cc2a4d889350305669f9eb515d26f2e11690c
Maria Elsa Tortello Ditta <maria.tortelloditta2@gmail.com>
2024-06-01 17:26:10 -0500

    ajuste de popups, nombres BEM,ajuste js file

--- scripts/aroundUS_proj5Spt5.js
+++ scripts/aroundUS_proj5Spt5.js
@@ -1,17 +1,52 @@
 let content = document.querySelector(".content");
-let editProfileContainer = content.querySelector(
-  ".popupWindows__editProfilePlace_btn_editProfile"
-);
-let editProfileBtn = content.querySelector(".profile__info-editNamePencil");
+let editProfileBtn = content.querySelector(".profile__info-edit");
 
-let addPlaceContainer = content.querySelector(
-  ".popupWindows__editProfilePlace_btn_addPlace"
-);
-let addImgBtn = content.querySelector(".profile__addProfile-sign");
+let addImgBtn = content.querySelector(".profile__add-sign");
 
-function editProfile() {}
+//Sección de declaración de funciones
+//Función para mostrar el popup de "editar perfil"
+function editProfile() {
+  document.querySelector(".popup_profile").classList.add("popup__show");
+}
 
-function addImage() {}
+//Función para mostrar el popup de "nuevo lugar"
+function addImage() {
+  document.querySelector(".popup_add-image").classList.add("popup__show");
+}
 
+//Función para cerrar los popups
+function closeAllPopups() {
+  document.querySelector(".popup_profile").classList.remove("popup__show");
+
+  document.querySelector(".popup_add-image").classList.remove("popup__show");
+}
+
+//Función para editar el popup "editar perfil"
+function submitProfile(event) {
+  event.preventDefault();
+  const nameNode = document.querySelector(".profile__info-name");
+  const inputName = document.querySelector(".form__edit-field_f1_name");
+  const aboutNode = document.querySelector(".profile__info-about");
+  const inputAbout = document.querySelector(".form__edit-field_f2_about");
+
+  if (inputName !== "" && inputAbout !== "") {
+    nameNode.textContent = inputName.value;
+    aboutNode.textContent = inputAbout.value;
+    closeAllPopups();
+  }
+}
+
+//Sección de llamado de funciones
 editProfileBtn.addEventListener("click", editProfile);
 addImgBtn.addEventListener("click", addImage);
+
+//Esta parte selecciona los botones X para cerrar los popups
+document.querySelectorAll(".popup__edit-close-btn").forEach((button) => {
+  button.addEventListener("click", closeAllPopups);
+});
+
+//Esta sección es para que la información del popup de "editar perfil"
+//se actualice en la pantalla principal
+document
+  .querySelector(".form_profile")
+  .addEventListener("submit", submitProfile);
