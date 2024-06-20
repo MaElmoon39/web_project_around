79cd912496725cd97b92b4a11f7df758151ecfbe
MaElmoon39 <maria.tortelloditta@gmail.com>
2024-06-20 05:51:39 -0500

    config final Card js class

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,63 +1,124 @@
-class Card {
-  constructor(cardName, cardLink, cardSelector) {}
-}
+import { keyHandler } from "./utils.js";
 
-function loadCards(name, link, alt) {
-  const cardTemplate = document.querySelector(
-    "#elements__card-template"
-  ).content;
+const openImage = document.querySelector(".popup_open-image");
 
-  const cardElement = cardTemplate
-    .querySelector(".elements__picture")
-    .cloneNode(true);
+export const initialCards = [
+  {
+    name: "Chinatown",
+    link: "images/Chinatown_ManhattanUSA.jpg",
+    alt: "Chinatown Manhattan USA",
+  },
 
-  const cardTitleElement = cardElement.querySelector(".elements__picture-name");
+  {
+    name: "Ghostbusters Headquarters",
+    link: "images/GhostbustersHeadquarters_NY_USA.jpg",
+    alt: "Ghostbusters Headquarters USA",
+  },
 
-  const cardImgElement = cardElement.querySelector(".elements__picture-size");
-  cardTitleElement.textContent = name;
-  cardImgElement.src = link;
-  cardImgElement.alt = alt || name;
+  {
+    name: "Guatapé",
+    link: "images/Guatape_AntioquiaCOL.jpg",
+    alt: "Guatape Antioquia Colombia",
+  },
 
-  //Esta sección permite activar el botón "me gusta"
-  const likeButton = cardElement.querySelector(".elements__like-btn");
-  likeButton.addEventListener("click", function (evt) {
-    evt.target.classList.toggle("elements__like-btn-active");
-  });
+  {
+    name: "Hong Kong",
+    link: "images/HongKong_Streetnight.jpg",
+    alt: "Streetnight HongKong",
+  },
 
-  //Esta sección permite eliminar la card
-  const trashButton = cardElement.querySelector(".elements__picture-trash-btn");
-  trashButton.addEventListener("click", () => {
-    cardElement.remove();
-  });
+  {
+    name: "Mochila Wayú",
+    link: "images/MochilaWayu_CartagenaCOL.jpg",
+    alt: "MochilaWayu Cartagena Colombia",
+  },
 
-  //Esta sección permite visualizar la imagen más grande, una vez se da click en la card
-  cardImgElement.addEventListener("click", () => {
-    openImage.classList.add("popup_opened", "popup-container-bg");
-    openImage.querySelector(".popup__big-img").src = link;
-    openImage.querySelector(".popup__big-img").alt = "Imagen de: " + name;
-    openImage.querySelector(".popup__big-img-name").textContent = name;
+  {
+    name: "Suculentas",
+    link: "images/suculentas.jpg",
+    alt: "Plantas de Suculentas Colombia",
+  },
+];
 
-    const popupContainer = document.querySelector(".popup-container");
-    popupContainer.classList.add("popup-container-bg");
-    document.addEventListener("keydown", keyHandler);
-  });
-
-  return cardElement;
-}
-
-const formElement = document.querySelector(".form_profile");
-const newImgForm = document.querySelector(".form_add-card");
-const formImgName = document.querySelector(".form__edit-field_image_name");
-const formImgLink = document.querySelector(".form__edit-field_image_link");
-
-function addNewCard(evt) {
-  evt.preventDefault();
-  const cardNode = loadCards(formImgName.value, formImgLink.value);
-
-  if (formImgName.value !== "" && formImgLink.value !== "") {
-    cardsContainer.prepend(cardNode);
-    closeAllPopups();
+export default class Card {
+  constructor(cardName, cardLink, cardAlt, cardContainer) {
+    this._cardName = cardName;
+    this._cardAlt = cardAlt;
+    this._cardLink = cardLink;
+    this._cardContainer = cardContainer;
   }
 
-  evt.target.reset();
+  //Esta función encuentra el template y procesa su contenido
+  _getTemplate() {
+    const cardTemplate = document
+      .querySelector("#elements__card-template")
+      .content.querySelector(".elements__picture")
+      .cloneNode(true);
+
+    return cardTemplate;
+  }
+
+  /* _toggleLikeBtn() {
+    this._likeButton = this._getTemplate();
+    this._likeButton.querySelector(".elements__like-btn");
+  }
+
+  _toggleTrashBtn() {
+    this._trashButton = this._getTemplate();
+    this._trashButton.querySelector(".elements__picture-trash-btn");
+  } */
+
+  //Esta función adiciona la información de nombre y link al template
+  generateCard() {
+    this._cardElement = this._getTemplate();
+    this._setEventListeners();
+
+    this._cardTitleElement = this._cardElement.querySelector(
+      ".elements__picture-name"
+    );
+
+    this._cardImgElement = this._cardElement.querySelector(
+      ".elements__picture-size"
+    );
+
+    this._cardTitleElement.textContent = this._cardName;
+    this._cardImgElement.src = this._cardLink;
+    this._cardImgElement.alt = this._cardAlt || this._cardName;
+
+    return this._cardElement;
+  }
+
+  //Esta función agrega todos los controladores de eventos (like/trash/openBigImg buttons)
+  _setEventListeners() {
+    this._button = this._getTemplate();
+    this._likeButton = this._button.querySelector(".elements__like-btn");
+    this._trashButton = this._button.querySelector(
+      ".elements__picture-trash-btn"
+    );
+    this._cardImgElement = this._cardElement.querySelector(
+      ".elements__picture-size"
+    );
+
+    this._trashButton.addEventListener("click", () => {
+      this._cardElement.remove();
+    });
+
+    this._likeButton.addEventListener("click", function (evt) {
+      evt.target.classList.toggle("elements__like-btn-active");
+    });
+
+    //Esta sección permite visualizar la imagen más grande, una vez se da click en una card
+    this._cardImgElement.addEventListener("click", () => {
+      openImage.classList.add("popup_opened", "popup-container-bg");
+      openImage.querySelector(".popup__big-img").src = this._cardLink;
+      openImage.querySelector(".popup__big-img").alt =
+        "Imagen de: " + this._cardName;
+      openImage.querySelector(".popup__big-img-name").textContent =
+        this._cardName;
+
+      const popupContainer = document.querySelector(".popup-container");
+      popupContainer.classList.add("popup-container-bg");
+      document.addEventListener("keydown", keyHandler);
+    });
+  }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 import { formProfileValidated } from "./FormValidator.js";
 import { closeAllPopups, closeHandler, keyHandler } from "./utils.js";
+import { initialCards } from "./Card.js";
+import Card from "./Card.js";
 
 const cardsContainer = document.querySelector(".elements");
 
@@ -8,103 +10,20 @@ const newImgForm = document.querySelector(".form_add-card");
 const formImgName = document.querySelector(".form__edit-field_image_name");
 const formImgLink = document.querySelector(".form__edit-field_image_link");
 
-const openImage = document.querySelector(".popup_open-image");
-
-const initialCards = [
-  {
-    name: "Chinatown",
-    link: "images/Chinatown_ManhattanUSA.jpg",
-    alt: "Chinatown Manhattan USA",
-  },
-
-  {
-    name: "Ghostbusters Headquarters",
-    link: "images/GhostbustersHeadquarters_NY_USA.jpg",
-    alt: "Ghostbusters Headquarters USA",
-  },
-
-  {
-    name: "Guatapé",
-    link: "images/Guatape_AntioquiaCOL.jpg",
-    alt: "Guatape Antioquia Colombia",
-  },
-
-  {
-    name: "Hong Kong",
-    link: "images/HongKong_Streetnight.jpg",
-    alt: "Streetnight HongKong",
-  },
-
-  {
-    name: "Mochila Wayú",
-    link: "images/MochilaWayu_CartagenaCOL.jpg",
-    alt: "MochilaWayu Cartagena Colombia",
-  },
-
-  {
-    name: "Suculentas",
-    link: "images/suculentas.jpg",
-    alt: "Plantas de Suculentas Colombia",
-  },
-];
-
-//Sección de declaración de funciones
-//Función para agregar las cards al cargar la página
-function loadCards(name, link, alt) {
-  const cardTemplate = document.querySelector(
-    "#elements__card-template"
-  ).content;
-
-  const cardElement = cardTemplate
-    .querySelector(".elements__picture")
-    .cloneNode(true);
-
-  const cardTitleElement = cardElement.querySelector(".elements__picture-name");
-
-  const cardImgElement = cardElement.querySelector(".elements__picture-size");
-  cardTitleElement.textContent = name;
-  cardImgElement.src = link;
-  cardImgElement.alt = alt || name;
-
-  //Esta sección permite activar el botón "me gusta"
-  const likeButton = cardElement.querySelector(".elements__like-btn");
-  likeButton.addEventListener("click", function (evt) {
-    evt.target.classList.toggle("elements__like-btn-active");
-  });
-
-  //Esta sección permite eliminar la card
-  const trashButton = cardElement.querySelector(".elements__picture-trash-btn");
-  trashButton.addEventListener("click", () => {
-    cardElement.remove();
-  });
-
-  //Esta sección permite visualizar la imagen más grande, una vez se da click en la card
-  cardImgElement.addEventListener("click", () => {
-    openImage.classList.add("popup_opened", "popup-container-bg");
-    openImage.querySelector(".popup__big-img").src = link;
-    openImage.querySelector(".popup__big-img").alt = "Imagen de: " + name;
-    openImage.querySelector(".popup__big-img-name").textContent = name;
-
-    const popupContainer = document.querySelector(".popup-container");
-    popupContainer.classList.add("popup-container-bg");
-    document.addEventListener("keydown", keyHandler);
-  });
-
-  return cardElement;
-}
-
 initialCards.forEach((data) => {
-  const cardNode = loadCards(data.name, data.link, data.alt);
-  cardsContainer.append(cardNode);
+  const cardNode = new Card(data.name, data.link, data.alt);
+  const cardElement = cardNode.generateCard();
+  cardsContainer.append(cardElement);
 });
 
 //Función para añadir nueva card desde el popup
 function addNewCard(evt) {
   evt.preventDefault();
-  const cardNode = loadCards(formImgName.value, formImgLink.value);
+  const cardNode = new Card(formImgName.value, formImgLink.value);
+  const cardElement = cardNode.generateCard();
 
   if (formImgName.value !== "" && formImgLink.value !== "") {
-    cardsContainer.prepend(cardNode);
+    cardsContainer.prepend(cardElement);
     closeAllPopups();
   }
 
